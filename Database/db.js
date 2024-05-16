8b8002e21bf454d2d2617a9291e6a950611edaa2
sagar.nandu <sagar.maskawar@werize.com>
2024-05-16 07:11:49 +0530

    env added

--- Database/db.js
+++ Database/db.js
@@ -1,10 +1,13 @@
 
 
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const DbConnection = async () => {
     // const MongoURL = `mongodb+srv://Abhishek:Abhishek1234@abhishek.zwylwnu.mongodb.net/?retryWrites=true&w=majority&appName=Abhishek`;
-  const MongoURL =`mongodb+srv://Adi:Adi1234@adi.xz9iatt.mongodb.net/?retryWrites=true&w=majority&appName=Adi`
+//   const MongoURL =`mongodb+srv://Adi:Adi1234@adi.xz9iatt.mongodb.net/?retryWrites=true&w=majority&appName=Adi`
+const MongoURL = process.env.MONGODB_URI || `mongodb+srv://Adi:Adi1234@adi.xz9iatt.mongodb.net/?retryWrites=true&w=majority&appName=Adi`;
     try {
         await mongoose.connect(MongoURL);
         console.log('Database connected successfully');
