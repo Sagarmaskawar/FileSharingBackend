d5181d52e0f37025d703290c3451b9e4481e090f
sagar.nandu <sagar.maskawar@werize.com>
2024-05-16 07:32:10 +0530

    env added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-import  express  from "express";
-import mongoose from "mongoose";
-import router from "./router/routes.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import router from './router/routes.js';
 import cors from 'cors';
-import DbConnection from "./Database/db.js";
-const app=express();
+import DbConnection from './Database/db.js';
+
+const app = express();
 app.use(cors());
-app.use('/',router);
-const PORT=8000;
+app.use('/', router);
+
+// Use the port provided by Heroku or default to 8000
+const PORT = process.env.PORT || 8000;
 
 DbConnection();
 
-app.listen(PORT, ()=> console.log(`app is statrted on port ${PORT}`));
+app.listen(PORT, () => console.log(`App is started on port ${PORT}`));
